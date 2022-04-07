76d24d26ebecd3deeb7d94c92d1dba0066549f7f
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-04-07 13:33:37 +0100

    - ADD: Added PWA stuff.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  pwa: {
+    name: 'SGONE',
+    themeColor: '#00a300',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    start_url: '/index.html',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 }
