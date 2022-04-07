61c1e6acb9b8a42c865dacb021aeed57ed12f4e0
sebastian-raubach <sebastian-raubach@users.noreply.github.com>
2022-04-07 12:19:24 +0000

    Deploying to gh-pages from @ cropgeeks/sgone@5d966e642ab0f979a80ad5fb21405207f2dc1558 🚀

---
+++ precache-manifest.3f8e0da2dc1a62277f036fd1dbdc970f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b97758d503fd688caf921f259dc16ac",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "a09f82edf444f07221fc73ae26242725",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "d50b79a22e08085f6b6fbc01794451b9",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "50fbafb31fef9b3398f6182fe2da742d",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "aae361ce78b29fc5d981",
+    "url": "css/app.768a4bdc.css"
+  },
+  {
+    "revision": "cd89f86dac94783217b9d3eed5df8f28",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "6a383ed6d62e8319c894c640eb9d0264",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "4aaf77eea08b34d27fec0b29441fa335",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "c91116ab1bc8194e5ca7618532e38bce",
+    "url": "index.html"
+  },
+  {
+    "revision": "aae361ce78b29fc5d981",
+    "url": "js/app.95c35dcd.js"
+  },
+  {
+    "revision": "cac1df1b83f16bbba851",
+    "url": "js/chunk-vendors.a327a274.js"
+  },
+  {
+    "revision": "66bee81aa776bc06f19b1fbb2401c55f",
+    "url": "levenshtein.worker.js"
+  },
+  {
+    "revision": "d11ca99594547b7778beddfd3a490575",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "881f5229f028eee2ffde7a8c34ff42a6",
+    "url": "mstile-144x144.png"
+  },
+  {
+    "revision": "83d0794d4f61cbb2b41246a43974b106",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "79e735e549651d1fcd0ffa7551d3f2e6",
+    "url": "mstile-310x150.png"
+  },
+  {
+    "revision": "607d8603476d6e659228842f232def67",
+    "url": "mstile-310x310.png"
+  },
+  {
+    "revision": "9d66109cfbc0e22d2cb22dea70ba5fac",
+    "url": "mstile-70x70.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "3b4608576b6814badde171ef8d1e1009",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "site.webmanifest"
+  }
+]);
